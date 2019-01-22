43e88b7e2db044821128087456b21bf27bf4c122
Stephen Wright <scw248@gmail.com>
2019-01-22 23:02:17 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    count = 0
+    array = self.split("." || "?" || "!")
+    array.each do |elem|
+      #binding.pry
+      count += 1
+    end
+    count
   end
 end
